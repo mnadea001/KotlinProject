b53e13941d990bbd293a335b4549aa3dd4bb7607
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-02-11 20:56:10 +0100

    2 btns on Home, first btn go to student list having 3 btns

--- app/src/main/java/com/example/mobileapp/HomeActivity.kt
+++ app/src/main/java/com/example/mobileapp/HomeActivity.kt
@@ -1,11 +1,20 @@
 package com.example.mobileapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val buttonStudentList=findViewById<Button>(R.id.buttonStudentList)
+        buttonStudentList.setOnClickListener(View.OnClickListener {
+            val intent= Intent(application,StudentListActivity::class.java)
+            startActivity(intent)
+        })
+
     }
 }
