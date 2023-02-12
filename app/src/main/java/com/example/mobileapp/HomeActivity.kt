934e3a6ffd726ea249fa1f59f2083532e2723072
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-02-12 17:08:07 +0100

    ajout style on splash and home : bg and btns

--- app/src/main/java/com/example/mobileapp/HomeActivity.kt
+++ app/src/main/java/com/example/mobileapp/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mobileapp
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,8 +12,13 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val buttonStudentList=findViewById<Button>(R.id.buttonStudentList)
+        val buttonCategoryList=findViewById<Button>(R.id.buttonCategoryList)
+//        add background on buttons
+        buttonStudentList.setBackgroundColor(Color.parseColor("#F5F749"))
+        buttonCategoryList.setBackgroundColor(Color.parseColor("#F5F749"))
+//        add event on click
         buttonStudentList.setOnClickListener(View.OnClickListener {
-            val intent= Intent(application,StudentListActivity::class.java)
+        val intent= Intent(application,StudentListActivity::class.java)
             startActivity(intent)
         })
 
