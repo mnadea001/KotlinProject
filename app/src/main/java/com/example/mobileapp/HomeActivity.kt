80d0a91364e5bfed2228f425dbf5010fd2e2cf45
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-02-12 17:47:23 +0100

    ajout hedaer on home

--- app/src/main/java/com/example/mobileapp/HomeActivity.kt
+++ app/src/main/java/com/example/mobileapp/HomeActivity.kt
@@ -6,11 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val textViewTitle=findViewById<TextView>(R.id.textViewTitle)
+
+        textViewTitle.setText("Home")
+
+
         val buttonStudentList = findViewById<Button>(R.id.buttonStudentList)
         val buttonCategoryList = findViewById<Button>(R.id.buttonCategoryList)
 //        add background on buttons
