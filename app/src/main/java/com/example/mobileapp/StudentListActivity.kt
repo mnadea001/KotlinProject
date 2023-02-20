658d104801078fc82b1cf28672a0a3a915e7a558
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-02-20 19:46:13 +0100

    add icon app

--- app/src/main/java/com/example/mobileapp/StudentListActivity.kt
+++ app/src/main/java/com/example/mobileapp/StudentListActivity.kt
@@ -2,7 +2,6 @@ package com.example.mobileapp
 
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
